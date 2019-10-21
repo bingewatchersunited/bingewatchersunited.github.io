388b5b2d8f2a71697f71e10547a7bab49d00ae4d
Trillnonymous <trillnonymous@gmail.com>
2019-10-20 22:07:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,18 @@
     BWUNavigation.prototype.constructor = BWUNavigation;
 
     (function(){
+        function noop(){}
+
+        this.makeClickFn = function(callback){
+            if (arguments.length === 0) callback = noop;
+
+            return function(event){
+                event.preventDefault();
+
+                callback.call(this, event);
+            };
+        };
+
         this.add = function(item){
             if (!this.$navigation.length) return;
 
@@ -15,16 +27,101 @@
                 handler = null, text = null, id = null, children = [];
             
             if ("handler" in item){
-                var h = item.handler;
-                if (typeof h === "function"){
-                    h = this.makeClickFn(h);
-                    $link.attr("href", "#").on("click", h);
+                handler = item.handler;
+                if (typeof handler === "function"){
+                    handler = this.makeClickFn(h);
+                    $link.attr("href", "#").on("click", handler.bind(this));
                 } else {
-                    $link.attr("href", h);
+                    $link.attr("href", handler);
                 }
             }
 
+            if (handler === null) $link.attr("href", "#").on("click", this.makeClickFn());
+
             $item.append($link);
+
+            if ("text" in item){
+                text = item.text;
+                $link.text(text);
+            }
+
+            if ("id" in item){
+                id = item.id;
+                $item.attr("id", id);
+            }
+
+            if ("children" in item){
+                if (item.children instanceof Array){
+                    var c = [].slice.call(item.children);
+
+                    while (c.length) children.push(c.shift());
+
+                    var $subnav = this.createSubnav(children);
+
+                    $item.append($subnav);
+                }
+            }
+        };
+
+        this.createSubnav = function(children, level){
+            if (typeof level !== "number" || level < 1) level = 1;
+
+            var wrapper_cl = "bwu-nav__subnav" + (level > 1 ? level : ""),
+                subnav_cl = "bwu-subnav" + (level > 1 ? level : ""),
+                $wrapper = $("<nav>", { "class": wrapper_cl }),
+                $subnav = $("<ul>", { "class": subnav_cl });
+            
+            $wrapper.append($subnav);
+            
+            var c = [].slice.call(children);
+
+            while (c.length){
+                var item = c.shift();
+
+                var $item = $("<li>", { "class": subnav_cl + "__item" }),
+                    $link = $("<a>", { "class": subnav_cl + "__link" }),
+                    handler = null, text = null, id = null, children = [];
+                
+                if ("handler" in item){
+                    handler = item.handler;
+                    if (typeof handler === "function"){
+                        handler = this.makeClickFn(h);
+                        $link.attr("href", "#").on("click", handler.bind(this));
+                    } else {
+                        $link.attr("href", handler);
+                    }
+                }
+    
+                if (handler === null) $link.attr("href", "#").on("click", this.makeClickFn());
+    
+                $item.append($link);
+    
+                if ("text" in item){
+                    text = item.text;
+                    $link.text(text);
+                }
+    
+                if ("id" in item){
+                    id = item.id;
+                    $item.attr("id", id);
+                }
+    
+                if ("children" in item){
+                    if (item.children instanceof Array){
+                        var c = [].slice.call(item.children);
+    
+                        while (c.length) children.push(c.shift());
+    
+                        var $subnavc = this.createSubnav(children, level++);
+    
+                        $item.append($subnavc);
+                    }
+                }
+
+                $subnav.append($item);
+            }
+
+            return $wrapper;
         };
     }).call(BWUNavigation.prototype);
 })(this === window ? this : window);
