0db6deba430d9279e8b9f5a0467f5def9156dd50
Trillnonymous <trillnonymous@gmail.com>
2019-11-12 20:00:24 -0500

    Update news.sql

--- news.sql
+++ news.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS bwu.news AS news (
     news_name VARCHAR (50) NOT NULL,
     news_text VARCHAR (1000) NOT NULL,
     news_image VARCHAR (255) NOT NULL,
-    news_type INT (2) NOT NULL,
+    news_type INT (2) NOT NULL, 
     made_on DATETIME
 );
