ba6ba9fb2152be3d4601576c454a26cd44f42f69
Trillnonymous <trillnonymous@gmail.com>
2019-12-12 02:42:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,164 +1,42 @@
 <?php
-	include "news/news.php";
-	include "";
-	
-	define(NAME, "Binge Watchers United");
-	define(ALIASES, array("BWU", "Television Lovers Club"));
-	define(KEYWORDS, array_merge(ALIASES, (array) NAME));
-	define(VERSION, "1.1.0 beta");
-	define(DESCRIPTION, "${name} is an organization geared towards students that need a safe space to show appreciation for television, film, and modern events related to media by attending meetings, participating in fun activities, and of course, having viewings.");
-	define(TITLE, "Welcome to ${name} - A Safe Place to Discuss Your Favorite Shows!!!");
-	$desc = constant("DESCRIPTION");
-	$name = constant("NAME");
-	$nav = [
-		"Home" => "index.php",
-		"News" => "news.php",
-		"Contact" => "contact.php",
-		"About" => "about.php",
-		"More" => [
-			"Timer" => "timer.php",
-			"Jeopardy Board" => "jeopardy-board.php"
-		]
-	];
+	define("NAME", "Binge Watchers United");
+	define("VERSION", "v1.0");
+
+	function description($name){
+		return `$name is a student organization chartered by Georgia State University. The purpose of the
+		organization is to foster a comfortable environment for students to view and have discussions about television
+		shows.`;
+	}
+
+	$title = constant("NAME");
+	$desc = description($title);
 ?>
 <!doctype html>
-<html class="BWUSite" id="BWUSite">
-	<head>
-		<title><?php echo TITLE; ?></title>
+<html class="BWUDocument bwu-document" id="bwu-document">
+	<head id="bwu-head">
+		<?php echo `<title>$title</title>`; ?>
+		<?php echo `<meta name="description" content="$desc" />`; ?>
 		<meta charset="UTF-8" />
-		<?php
-			$j = implode(", ", KEYWORDS);
-			echo "<meta name=\"keywords\" content=\"${j}\" />";
-			echo "<meta name=\"description\" content=\"${desc}\" />";
-		?>
+		<meta name="keywords" content="BWU, Binge Watchers United, binge watchers united, Television and Streaming Club" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="shortcut icon" href="BWULogo.png" type="image/png" sizes="8x8 16x16 32x32 64x64" />
+		<link rel="apple-touch-icon" sizes="32x32" href="BWULogo.png" type="image/png" />
 		<link rel="stylesheet" href="index.css" type="text/css" />
-		<link rel="manifest" href="manifest.json" type="text/json" />
-		<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-		<script src="index.js"></script>
-		<script src="main.js"></script>
+		<script src="index.js" type="application/javascript"></script>
 	</head>
-	<body class="BWUContent" id="BWUContent">
-		<div class="BWUContentWrapper" id="BWUContentWrapper">
-			<header class="BWUHeader" id="BWUHeader">
-				<h1 class="BWUHeading" id="BWUHeading">
-					<a href="index.php" class="BWULogoLink bwu-logo__link" id="BWULogoLink">
-						<img src="BWULogo.png" class="BWULogoImage bwu-logo__image" id="BWULogoImage" />
-						<span class="BWULogoText" id="BWULogoText">
-							<?php echo constant("NAME"); ?>
-						</span>
-					</a>
-				</h1>
-				<nav class="BWUNavigation bwu-navigation" id="BWUNavigation">
-					<ul class="BWUNav bwu-nav" id="BWUNav">
-						<?php 
-							foreach ($nav as $t => $a){
-								if (!isset($a) || !isset($t)) continue;
-
-								if ($a === "" || $t === "") continue;
-
-								echo "<li class=\"BWUNavItem bwu-nav__item\">";
-								
-								if (is_object($a) || is_array($a)){
-									$link = "#";
-
-									if (isset($a["__link"])){
-										$ol = $link;
-										$link = $a["__link"];
-
-										if ($link === "" || !isset($link) || !is_string($link)){
-											$link = $ol;
-										}
-
-										unset($ol, $a["__link"]);
-									}
-
-									echo "<a href=\"${link}\" class=\"BWUNavLink bwu-nav__link\">${t}</a>";
-									echo "<section class=\"BWUNavSubnav bwu-nav__subnav\"><ul class=\"BWUSubnav bwu-subnav\">";
-
-									foreach ($a as $k => $v){
-
-										if (!isset($k) || !isset($v)) continue;
-
-										if ($v === "" || $k === "") continue;
-
-										echo "<li class=\"BWUSubnavItem bwu-subnav__item\">";
-
-										if (is_object($v) || is_array($v)){
-											$l = "#";
-
-											if (isset($v["__link"])){
-												$olink = $l;
-												$l = $v["__link"];
-
-												if ($l === "" || !isset($l) || !is_string($l)){
-													$l = $olink;
-												}
-
-												unset($olink, $v["__link"]);
-											}
-
-											echo "<a href=\"${l}\" class=\"BWUSubnavLink bwu-subnav__link\">${k}</a>";
-											echo "<section class=\"BWUNavSubnav2 bwu-nav__subnav2\"><ul class=\"BWUSubnav2 bwu-subnav2\">";
-
-											foreach ($v as $key => $value){
-												if (!isset($value) || !isset($key)) continue;
-
-												if ($value === "" || $key === "") continue;
-
-												echo "<li class=\"BWUSubnav2Item bwu-subnav2__item\">";
-												
-												if (!is_string($value)){
-													$value = "#";
-												}
-
-												echo "<a href=\"${value}\" class=\"BWUSubnav2Link bwu-subnav2__link\">${key}</a>";
-												echo "</li>";
-
-												unset($value);
-											}
-
-											echo "</ul></section>";
-										} else {
-											if (!is_string($v)){
-												$v = "#";
-											}
-
-											echo "<a href=\"${v}\" class=\"BWUSubnavLink bwu-subnav__link\">${k}</a>";
-										}
-
-										echo "</li>";
-										unset($v);
-									}
-
-									echo "</ul></section>";
-								} else {
-									if (!is_string($a)){
-										$a = "#";
-									}
-
-									echo "<a href=\"${a}\" class=\"BWUNavLink bwu-nav__link\">${t}</a>";
-								}
-
-								unset($a);
-							}
-						?>
-					</ul>
-				</nav>
-				<form class="BWUSearch bwu-search" id="BWUSearch">
-					<div class="BWUSearchbox bwu-searchbox" id="BWUSearchbox">
-						<label class="BWUSearchLabel bwu-search__label" id="BWUSearchLabel" for="BWUSearchInput">Search</label>
-						<input class="BWUSearchInput bwu-search__input" id="BWUSearchInput" type="text" />
-						<a href="#BWUSearchInput" class="BWUSearchButton bwu-search__button" id="BWUSearchButton">
-							<svg class="BWUSearchIcon bwu-search__icon" id="BWUSearchIcon" viewbox="0 0 24 24">
-								<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-								<path d="M0 0h24v24H0z" fill="none" />
-							</svg>
-						</a>
-					</div>
-					<nav class="BWUAutocomplete bwu-autoomplete" id="BWUAutocomplete"></nav>
-				</form>
-			</header>
+	<body class="BWUBody bwu-body" id="bwu-body">
+		<div class="BWUContentWrapper bwu-content__wrapper">
+			<figure class="BWULogoContainer bwu-logo__container">
+				<div class="BWULogoWrapper bwu-logo__wrapper">
+					<picture class="BWULogo bwu-logo">
+						<source srcset="BWULogo.png" media="(min-width: 300px)" />
+						<img src="BWULogo.png" alt="Binge Watchers United" />
+					</picture>
+				</div>
+				<figcaption class="BWUIndicator bwu-indicator">
+					We are now loading content for you guys. We appreciate your patience!
+				</figcaption>
+			</figure>
 		</div>
 	</body>
 </html>
