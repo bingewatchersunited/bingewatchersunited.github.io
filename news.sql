f357531497851591395097d46564e3ae6a2db245
bingewatchersunited <56529284+bingewatchersunited@users.noreply.github.com>
2019-12-12 01:52:36 -0500

    Delete news.sql

--- news.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS bwu.news AS news (
-    id INT PRIMARY KEY IDENTITY (1,1),
-    news_name VARCHAR (50) NOT NULL,
-    news_text VARCHAR (1000) NOT NULL,
-    news_image VARCHAR (255) NOT NULL,
-    news_type INT (2) NOT NULL, 
-    made_on DATETIME
-);
